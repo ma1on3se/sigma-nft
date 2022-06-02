5b2383c4821dcb26e271f9dd2cd213111e4aaf61
Otávio Cardoso <81239817+ma1on3se@users.noreply.github.com>
2022-06-02 12:28:11 -0300

    chore(*): inclusão de dados

---
+++ includes/_dados.php
@@ -0,0 +1,15 @@
+<?php
+$produtos = array(
+    0 => array('nome'=>'Gorila Legal','preco'=>'6720.00','descricao'=>'Gorila Legal','imagem'=>'gorila-legal.jpeg'),
+    1 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
+    2 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
+    3 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
+    4 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
+    5 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
+    6 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
+    7 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
+    8 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
+    9 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
+    10 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>'')
+);
+?>
