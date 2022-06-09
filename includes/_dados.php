a65cd730e227052f00d17c9bb90fe808e8482545
Otávio Cardoso <81239817+otaviocardosooo@users.noreply.github.com>
2022-06-09 12:18:42 -0300

    chore(*): adição de dados e manipulação dos arrays

--- includes/_dados.php
+++ includes/_dados.php
@@ -1,15 +1,14 @@
 <?php
 $produtos = array(
-    0 => array('nome'=>'Gorila Legal','preco'=>'6720.00','descricao'=>'Gorila Legal','imagem'=>'gorila-legal.jpeg'),
-    1 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
-    2 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
-    3 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
-    4 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
-    5 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
-    6 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
-    7 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
-    8 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
-    9 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>''),
-    10 => array('nome'=>'','preco'=>'','descricao'=>'','imagem'=>'')
+    0 => array('nome'=>'Cristo BoredApe','preco'=>'1199.00','descricao'=>'BoredApe inspirado no Inri Cristo','imagem'=>'cristo-ape.jpg'),
+    1 => array('nome'=>'GoldRainbow BoredApe','preco'=>'9100.00','descricao'=>'BoredApe dourado','imagem'=>'goldrainbow-ape.jpg'),
+    2 => array('nome'=>'King BoredApe','preco'=>'3450.00','descricao'=>'BoredApe colorido com coroa','imagem'=>'king-ape.jpg'),
+    3 => array('nome'=>'YellowDrip BoredApe','preco'=>'25000.00','descricao'=>'BoredApe amarelo com roupas de marca','imagem'=>'yellowdrip-ape.jpg'),
+    4 => array('nome'=>'Albino BoredApe','preco'=>'2100.00','descricao'=>'BoredApe albino','imagem'=>'albino-ape.jpg'),
+    5 => array('nome'=>'Gold BoredApe','preco'=>'90000.00','descricao'=>'BoredApe dourado','imagem'=>'gold-ape.jpg'),
+    6 => array('nome'=>'Russian BoredApe','preco'=>'5010.00','descricao'=>'BoredApe russo','imagem'=>'russian-ape.jpg'),
+    7 => array('nome'=>'Trash BoredApe','preco'=>'7500.00','descricao'=>'BoredApe derretido','imagem'=>'trash-ape.jpg'),
+    8 => array('nome'=>'Demon BoredApe','preco'=>'1119.00','descricao'=>'BoredApe demônio','imagem'=>'demon-ape.jpg'),
+    9 => array('nome'=>'LSD BoredApe','preco'=>'8500.00','descricao'=>'BoredApe com pelos coloridos','imagem'=>'lsd-ape.jpg')
 );
 ?>
